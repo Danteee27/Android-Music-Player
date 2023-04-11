712a5b8dcf5bb5303780f7bae99952ce2f2acc87
Nguyen Ngoc Anh Khoa <73281000+anhkhoajjo@users.noreply.github.com>
2023-04-11 20:32:14 +0700

    error at dagger hilt

---
+++ app/src/main/java/com/example/myapplication/di/AppMoudule.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication.di
+
+import android.app.Application
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.myapplication.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Application::class)
+object AppMoudule {
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+       @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+            .error(R.drawable.ic_launcher_foreground)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+        //note: in instruction ic_laucher_foreground = ic_image
+    )
+}
