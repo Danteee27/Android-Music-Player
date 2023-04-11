59e38b0e38f9a705eb7e8dd39e138cf093ec9e1a
Nguyen Ngoc Anh Khoa <73281000+anhkhoajjo@users.noreply.github.com>
2023-04-11 17:27:51 +0700

    setup Test Collection and create constant for collection in DB

---
+++ app/src/main/java/com/example/myapplication/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.data.entities
+
+data class Song (
+    val mediaID: String ="",
+    val title: String ="",
+    val subTitle: String ="",
+    val songUrl: String ="",
+    val imageUrl: String ="",
+)
