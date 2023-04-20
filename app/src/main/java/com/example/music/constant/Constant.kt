9ff70a2de58cc05ca88ea6faf68c7790ebc4f4a2
Nguyen Ngoc Anh Khoa <73281000+anhkhoajjo@users.noreply.github.com>
2023-04-20 22:53:17 +0700

    Create Constant.kt

---
+++ app/src/main/java/com/example/music/constant/Constant.kt
@@ -0,0 +1,30 @@
+package com.example.music.constant
+
+object Constant {
+
+    const val PAGE_FACEBOOK: String = ""
+    const val LINK_FACEBOOK: String = ""
+    const val LINK_YOUTUBE: String = ""
+    const val PHONE_NUMBER: String = ""
+    const val GMAIL: String = ""
+    const val SKYPE_ID: String = ""
+    const val ZALO_LINK: String = ""
+
+    // Firebase url
+    const val FIREBASE_URL: String = "https://mobile-music-player-fddd1-default-rtdb.firebaseio.com"
+    // Max count
+    const val MAX_COUNT_BANNER = 5
+    const val MAX_COUNT_POPULAR = 4
+    const val MAX_COUNT_LATEST = 4
+
+    // Music actions
+    const val PLAY = 0
+    const val PAUSE = 1
+    const val NEXT = 2
+    const val PREVIOUS = 3
+    const val RESUME = 4
+    const val CANNEL_NOTIFICATION = 5
+    const val MUSIC_ACTION: String = "musicAction"
+    const val SONG_POSITION: String = "songPosition"
+    const val CHANGE_LISTENER: String = "change_listener"
+}
